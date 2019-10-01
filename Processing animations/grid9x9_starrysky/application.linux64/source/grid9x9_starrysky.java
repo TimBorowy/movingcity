9c16839ca499dba047e4e10efe33ef562bbdd8d8
Tim Borowy <blastertimbo@gmail.com>
2019-10-01 20:33:49 +0200

    export app for linux

--- Processing animations/grid9x9_starrysky/application.linux64/source/grid9x9_starrysky.java
+++ Processing animations/grid9x9_starrysky/application.linux64/source/grid9x9_starrysky.java
@@ -18,53 +18,75 @@ import java.io.IOException;
 public class grid9x9_starrysky extends PApplet {
 
 OPC opc;
-PImage aquarius;
+PImage[] aquarius = new PImage[5];
+PImage currentImage;
+int loop;
+int cycle;
 
 public void setup()
 {
   
-  frameRate(24);
+  frameRate(20);
 
-  aquarius = loadImage("aquarius.jpg");
-  //aquarius = loadImage("waterman2.jpg");
-  //aquarius = loadImage("waterman3.jpg");
-  aquarius = loadImage("aquarius_orange.jpg");
+  aquarius[0] = loadImage("data/aquarius_pink.jpg");
+  aquarius[1] = loadImage("data/aquarius_red.jpg");
+  aquarius[2] = loadImage("data/aquarius_purple.jpg");
+  aquarius[3] = loadImage("data/aquarius_blue.jpg");
+  aquarius[4] = loadImage("data/aquarius_orange.jpg");
+  
+  currentImage = aquarius[0];
+  loop = 0;
+  cycle = 1;
 
   // Connect to the local instance of fcserver. You can change this line to connect to another computer's fcserver
   opc = new OPC(this, "192.168.1.25", 7890);
   opc.ledGrid(0, 9, 9, width/2, height/2, 50, 50, 0, true);
 }
 
-float px, py;
-
 public void draw()
 {
+  loop++;
+  
   background(0);
-  //blendMode(ADD);
   
-  // crude animation "speed"
-  float a = millis() * 0.00002f % 360;
-  
-  //float r = py * 0.5;
-  //float dx = width/2 + cos(a) * r;
-  //float dy = height/2 + sin(a) * r;
+  // Defines how long a loop is
+  float frame = loop % 3601;
 
   // image size
   float size = 480;
-  
-  // tryout for color animation
-  float greenHue = millis() * 0.1f % 360;
-  float redHue =  millis() * 0.1f % 360;
-  
+
   imageMode(CENTER);
   
-  // rotate and translate the actual image
-  translate(450/2,450/2); 
-  rotate(a); 
-  //tint(260, 100, 50, 255);
-  image(aquarius, 0, 0, size, size);
-  // color the image
+  // Rotate and translate the actual image
+  translate(450/2, 450/2);
+  float rad = radians(frame/10);
+  rotate(rad);
   
+  // Change hue of image
+  // tint(260, 100, 50, 255);
+  
+  // Once a loop, select next image
+  if(frame == 3600){
+    
+    // 5 equals to amount of items in the image array
+    int i = cycle++ % 5; 
+    currentImage = aquarius[i];
+  }
+  
+  image(currentImage, 0, 0, size, size);
+  
+  // Speed
+  delay(1);
+}
+
+public void stop(){
+  // ToDo: before exiting, turn all pixels off.
+  int pixels = 9*9;
+
+  for(int i = 0; i < pixels; i++){
+    opc.setPixel(i, color(0,0,0));
+  }
+  opc.writePixels();
 }
 /*
  * Simple Open Pixel Control client for Processing,
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# movingcity
+# Movingcity
 A code repository for all animations, configs and other stuffs that has to do with my fadecandy artproject
+
+## Setting up your PI
+
+Uses [BerryLan](https://github.com/nymea/berrylan) for quick and easy network setup.
+
+Flash the image on an SD card using Balena Etcher
+
+Follow the fadecandy server setup over at 
+[adafruit.com](https://learn.adafruit.com/1500-neopixel-led-curtain-with-raspberry-pi-fadecandy/fadecandy-server-setup)
+
+use the serverconfig.json file as config for the server. You need to change the serial to your own fadecandy serial string.
+
+### Other things
+
+-   Change password
+-   Set rasbian to CLI and,
+-   Enter the command `sudo raspi-config` . Scroll down to Boot Options and select Console Autologin. Then exit the configuration menu and reboot.
+
+
+### Links:
+
+Fadecandy disconnects, maybe non clean power
+
+https://github.com/scanlime/fadecandy/issues/112
+
+How to test for clean power
+
+https://forum.arduino.cc/index.php?topic=476877.0
+
+Pi accesspoint
+
+https://www.raspberrypi.org/documentation/configuration/wireless/access-point.md
+
+
+
+
+
